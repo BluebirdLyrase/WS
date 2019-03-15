cc2805546cb1f5bf119fe5b8436a437535ce0363
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-03-15 20:07:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,37 +7,49 @@ function showOutput(resultText,pic,picText){
 
 
 $(function(){
-    var pre_names = ['A','B','C','D','E'];
-    var post_names = ['1','2','3','4','5'];
     $('#go').click(function(){
-        $('#go').hide(); 
-        $('#imgResult').hide() 
-        $('#go').fadeIn(800); 
-        $('#imgResult').fadeIn(800);
+        $('#pic').hide(); 
+        $('#pic').fadeIn(500);
+
         ///////////////data////////////////////////////
         var day = $('#day').val();
         var month = $('#month').val();
         var year = $('#year').val();
         var integer = parseInt(year)
         console.log(day + ',' + month);
-        var ArrayPic = ['pic/1.jpg','pic/2.jpg','pic/3.jpg','pic/4.jpg','pic/5.jpg','pic/6.jpg','pic/7.jpg']
+        var ArrayPic = ['pic/1.jpg','pic/2.jpg','pic/3.jpg','pic/4.jpg','pic/5.jpg','pic/6.jpg','pic/7.jpg'];
         var rdmPic = Math.floor(Math.random() * ArrayPic.length);
-        var ArrayPicText = ['You know this is just a website right? a student project website.',
-        'Ever heard of Lynks disease? That might be the cause of your death',
-        'never like any front end work...sadly',
-        'I suppose to do a good website for better score.What on earth am i doing here?',
-        'lifes toughest problems do not have simple answers. You should not kill yourself',
-        'Hope this will has any fun? HAH!!!',
-        'It is her idea to change this part of this website.I hope this has any meaning to her.',
-        'It is her idea to change this part of this website.I hope this has any meaning to her.'
-        ]
+        var ArrayPicText = ['Goodnight teacher or fellow student.',
+        'Ever heard of Lynks disease?',
+        'Please turn off ad-block to enjoy the music',
+        'DONR EVER ANSWER THE PHONE. DONT MAKE MY MISTAKE!',
+        'lifes toughest problems do not have simple answers.',
+        '4 have been shot but one were missing',
+        '4 have been shot but one were missing',
+        'Sometime I saw a weird pink man.',
+        'Maybe it is just a hallucination.She said',
+        'Dad is holding a shogun at us',
+        'I rush in thier room because I heard 2 gunshots',
+        'It is not my falut.I HAVE THAT DISEASE. I KNOW IT!',
+        'I do not know how to pick up a blue chair off the ground'
+        ];
+        ////////////////////////Random and calculate///////////////////////////////
         var rdmPicText = Math.floor(Math.random() * ArrayPicText.length);
         var date = new Date();
         var currentYear = date.getFullYear();
         var live = year-1941;
         var rdmYear = Math.floor(Math.random() * live);
         var pic = Math.floor(Math.random() * live);
+        var time = date.getHours()
+        console.log(time);
 
+        if(time>=5&&time<12){
+            ArrayPicText[0] = "Good moring teacher or fellow friend."
+        }else if(time>=12&&time<18){
+            ArrayPicText[0] = "Good afternoon teacher or fellow friend."
+        }else if(time>=18&&time<22){
+            ArrayPicText[0] = "Good evening teacher or fellow friend."
+        }
         ////////////////////////////////////////////////////////
 
         ///////////Special////////////
@@ -56,7 +68,6 @@ $(function(){
             $('#bar3').css("color","white");
             $('#result').css("color","white");
             $('#html').css("background-image", "url(pic/birthday.png)");  
- 
             showOutput("We have the same birthday",'pic/gif.gif'," ");
         }
       
@@ -82,7 +93,7 @@ $(function(){
             
             else if(rdmYear==1){
 
-            showOutput("You have only onr year left",ArrayPic[rdmPic],"make it count");    
+            showOutput("You have only one year left",ArrayPic[rdmPic],"make it count");    
 
             }else{
 
